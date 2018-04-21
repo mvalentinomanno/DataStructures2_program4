669bc03a5b06d78776acce09cb88c55aa522a2e5
mvalentinomanno <mvalentinomanno@gmail.com>
2018-04-21 15:34:41 -0600

    got register and 2 jtests working

--- test/dynamicRegisterTest.java
+++ test/dynamicRegisterTest.java
@@ -12,63 +12,36 @@ public class dynamicRegisterTest {
 
     @Test
     public void test1() { //tests 1,2,3 test the greedy register method with different inputs
-        int coins[] = {1, 5, 10, 25};//coins in use
-        String result[] = {"25", "25", "10", "5", "1", "1", "1"}; //expected result
-        int total = 68;
+        int coins[] = {1, 5, 12, 25};//coins in use
+        int expectedAns[] = {1,5,5,5};
+        int total = 16;
         int count = 0;
+        LinkedList ll = dynamicRegister.cashReg(coins, total);
 
-        LinkedList ll = dynamicRegister.cashReg(coins, total); //output list of coins
-        String coinsReturned[] = new String[ll.size()];
-
+        int coinsReturned[] = new int[ll.size()];
         while (!ll.isEmpty()) {
             Object u = ll.remove();
-            coinsReturned[count] = u.toString(); //convert list to array to compare
+            coinsReturned[count] = Integer.parseInt(u.toString());
             count++;
         }
-
-            Assert.assertArrayEquals(result, coinsReturned); //fail if expected array is not equal to output
-
+        
+        for(int i = 0; i < expectedAns.length; i++){
+            Assert.assertEquals(expectedAns[i], coinsReturned[i]);
+        }
     }
 
+    
     @Test
     public void test2() {
-        int coins[] = {1, 5, 10, 25};
-        String result[] = {"25", "10", "10", "1", "1", "1", "1"};
-        int total = 49;
-        int count = 0;
-
-        LinkedList ll = dynamicRegister.cashReg(coins, total);
-        String coinsReturned[] = new String[ll.size()];
-
-        while (!ll.isEmpty()) {
-            Object u = ll.remove();
-            coinsReturned[count] = u.toString();
-            count++;
-        }
-
-            Assert.assertArrayEquals(result, coinsReturned);
+        
     }
 
     @Test
     public void test3() {
-        int coins[] = {1, 5, 10, 25 , 8};
-        String result[] = {"10", "5", "1"};
-        int total = 16;
-        int count = 0;
-
-        LinkedList ll = dynamicRegister.cashReg(coins, total);
-        String coinsReturned[] = new String[ll.size()];
-
-        while (!ll.isEmpty()) {
-            Object u = ll.remove();
-            coinsReturned[count] = u.toString();
-            count++;
-        }
-
-            Assert.assertArrayEquals(result, coinsReturned);
+       
     }
-
-    @Test
+     
+    @Test(expected = IllegalArgumentException.class)
     public void testEmptyArray() { //greedy register throws illegal argument if array is size 0
         int coins[] = new int[0];
         int total = 68;
