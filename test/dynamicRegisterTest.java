6ad07655b7236861011f574ec397fef620064a56
Benjamin Seal <36999463+xJohnnyBravo@users.noreply.github.com>
2018-04-24 15:08:36 -0600

    Added 2 additional jtests

--- test/dynamicRegisterTest.java
+++ test/dynamicRegisterTest.java
@@ -33,11 +33,43 @@ public class dynamicRegisterTest {
     
     @Test
     public void test2() {
+        int coins[] = {1, 5, 13, 17};//coins in use
+        int expectedAns[] = {13,13,13};
+        int total = 39;
+        int count = 0;
+        LinkedList ll = dynamicRegister.cashReg(coins, total);
+
+        int coinsReturned[] = new int[ll.size()];
+        while (!ll.isEmpty()) {
+            Object u = ll.remove();
+            coinsReturned[count] = Integer.parseInt(u.toString());
+            count++;
+        }
+        
+        for(int i = 0; i < expectedAns.length; i++){
+            Assert.assertEquals(expectedAns[i], coinsReturned[i]);
+        }
         
     }
 
     @Test
     public void test3() {
+        int coins[] = {1, 3, 7, 10};//coins in use
+        int expectedAns[] = {7,7,7};
+        int total = 21;
+        int count = 0;
+        LinkedList ll = dynamicRegister.cashReg(coins, total);
+
+        int coinsReturned[] = new int[ll.size()];
+        while (!ll.isEmpty()) {
+            Object u = ll.remove();
+            coinsReturned[count] = Integer.parseInt(u.toString());
+            count++;
+        }
+        
+        for(int i = 0; i < expectedAns.length; i++){
+            Assert.assertEquals(expectedAns[i], coinsReturned[i]);
+        }
        
     }
      
